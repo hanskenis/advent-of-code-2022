4e93298e4d4ee617e4adfe3b9e7cdb002e604402
Hans Kenis <hans@kabisa.nl>
2022-12-07 07:29:33 +0100

    Day07.java

---
+++ java/Day07.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day07 {
+    class TreeNode {
+        int size;
+        String name;
+        List<TreeNode> children;
+        TreeNode parent;
+
+        public TreeNode(int size, String name) {
+            this.size = size;
+            this.name = name;
+            this.children = new ArrayList<>();
+        }
+
+        public void addChild(TreeNode child) {
+            this.children.add(child);
+            child.parent = this;
+        }
+
+        public int totalSize() {
+            return this.children.stream().mapToInt(x -> x.totalSize()).sum() + this.size;
+        }
+
+    }
+
+    TreeNode root = new TreeNode(0, "/");
+
+    public Day07(List<String> input) {
+        var currentNode = root;
+        for (String line : input) {
+            if (line.startsWith("$ cd /")) {
+                continue;
+            } else if (line.startsWith("$ ls")) {
+                continue;
+            } else if (line.startsWith("$ cd ..")) {
+                currentNode = currentNode.parent;
+            } else if (line.startsWith("$ cd")) {
+                var name = line.substring(5);
+                currentNode = currentNode.children.stream().filter(x -> x.name.equals(name)).findFirst()
+                        .get();
+            } else {
+                var splitted = line.split(" ");
+                if (splitted[0].equals("dir")) {
+                    currentNode.addChild(new TreeNode(0, splitted[1]));
+                } else {
+                    currentNode.addChild(new TreeNode(Integer.parseInt(splitted[0]), splitted[1]));
+                }
+            }
+        }
+    }
+
+    private int part1() {
+        var total = 0;
+        List<TreeNode> nodesTodo = new ArrayList<TreeNode>();
+        nodesTodo.add(root);
+        List<TreeNode> nodesDone = new ArrayList<TreeNode>();
+        while (!nodesTodo.isEmpty()) {
+            var currentNode = nodesTodo.remove(0);
+            var nodeTotal = currentNode.totalSize();
+            if (nodeTotal > 0 && nodeTotal <= 100000 && currentNode.size == 0) {
+                total += nodeTotal;
+            }
+            nodesDone.add(currentNode);
+
+            for (var child : currentNode.children) {
+                if (!nodesDone.contains(child)) {
+                    nodesTodo.add(child);
+                }
+            }
+        }
+
+        return total;
+
+    }
+
+    private int part2() {
+        var min = Integer.MAX_VALUE;
+        List<TreeNode> nodesTodo = new ArrayList<TreeNode>();
+        nodesTodo.add(root);
+        List<TreeNode> nodesDone = new ArrayList<TreeNode>();
+        var unusedSpace = 70000000 - root.totalSize();
+
+        while (!nodesTodo.isEmpty()) {
+            var currentNode = nodesTodo.remove(0);
+            var nodeTotal = currentNode.totalSize();
+            if (unusedSpace + nodeTotal >= 30000000 && currentNode.size == 0) {
+                min = Math.min(min, nodeTotal);
+            }
+            nodesDone.add(currentNode);
+
+            for (var child : currentNode.children) {
+                if (!nodesDone.contains(child)) {
+                    nodesTodo.add(child);
+                }
+            }
+        }
+
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        List<String> data = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            data.add(scanner.nextLine());
+        }
+
+        var day = new Day07(data);
+
+        System.out.println("Part 1: " + day.part1());
+        System.out.println("Part 2: " + day.part2());
+
+        scanner.close();
+    }
+}
